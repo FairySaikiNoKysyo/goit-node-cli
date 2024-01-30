fcf7f278e263361862582b024cf956d8b42d83ce
FairySaikiNoKysyo <michaelevgeniymyers@gmail.com>
2024-01-30 10:55:02 +0200

    add

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const invokeAction = async({ action, contactId, name, email, phone }) =>{
   switch (action) {
     case "list":
       const allContacts = await contactsService.listContacts();
-      console.log(allContacts);
+      console.table(allContacts);
       break;
 
     case "get":
